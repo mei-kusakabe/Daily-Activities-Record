da9df360fc9bfff0f6d3d6b4e55e67d99407a218
Jafrin <jafrin3347@gmail.com>
2022-09-29 09:54:22 +0600

    second-commit

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './Activities.css'
 import Activity from '../Activity/Activity';
+import Cart from '../Cart/Cart';
 
 const Activities = () => {
 
     const [activities, setActivities] = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('activities.json')
             .then(res => res.json())
@@ -12,21 +14,34 @@ const Activities = () => {
                 setActivities(data);
             })
     }, [])
+
+    const handleAddToList = (activity) => {
+        console.log(activity);
+
+        let newCart = [...cart, activity];
+        setCart(newCart)
+
+    }
     return (
         <div>
             <p>Select Today's Activities</p>
-
             <div className='activities-container'>
-                <div className="activitiy-container">
+
+                <div className="activity-container">
                     {
-                        activities.map(activity => <Activity key={activity.id}></Activity>)
+                        activities.map(activity => <Activity
+                            key={activity.id}
+                            activity={activity}
+                            handleAddToList={handleAddToList}
+                        ></Activity>)
                     }
                 </div>
                 <div className="cart-container">
-                    <h2>This is for cart</h2>
+                    <Cart cart={cart}></Cart>
                 </div>
             </div>
-        </div >
+        </div>
+
     );
 };
 
--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Activity.css';
 
-const Activity = () => {
+const Activity = ({ activity, handleAddToList }) => {
+    // console.log(props.activity)
+    // const { activity, handleAddToList } = props
+    const { name, img, description, duration } = activity
+    // console.log(props)
     return (
-        <div>
-            <h2>This is single activity</h2>
+        <div className='activity'>
+            <img src={img} alt=""></img>
+            <div className='activity-info'>
+                <p className='activity-name'>{name}</p>
+                <p><small>About: {description}</small></p>
+                <p><small>Time Required: {duration}</small></p>
+            </div>
+            <button onClick={() => handleAddToList(activity)} className='btn-cart'>
+                <p className='btn-text'>Add to List</p>
+            </button>
         </div>
     );
 };
